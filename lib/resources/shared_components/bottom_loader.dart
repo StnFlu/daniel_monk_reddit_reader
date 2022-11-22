860d89717fb99eacd923644c961e086d3ec6638c
Daniel Monk <daniel.monk@outlook.com>
2022-11-22 00:03:59 +0000

    render bloc elements to page

---
+++ lib/resources/shared_components/bottom_loader.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomLoader extends StatelessWidget {
+  const BottomLoader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 32.0),
+      child: Column(
+        children: const [
+          CircularProgressIndicator(),
+          Padding(
+            padding: EdgeInsets.only(top: 12),
+            child: Text("Fetching more posts..."),
+          )
+        ],
+      ),
+    );
+  }
+}
